359d3b4bb15603f81a91277e4bdcb019c87c3dff
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-19 13:37:17 +0000

    Rename utils func name

--- Shell/shell.h
+++ Shell/shell.h
@@ -63,21 +63,21 @@ void free_last_input(void);
 void _puts(char *str);
 void _puterror(char *err);
 
-/* utils_funcs1.c */
+/* helpers_1.c */
 int _strlen(const char *);
 int _strcmp(const char *s1, const char *s2);
 int _strncmp(const char *s1, const char *s2, size_t n);
 char *_strstr(char *haystack, char *needle);
 char *_strchr(char *s, char c);
 
-/* utils_funcs2.c */
+/* helpers_2.c */
 char *_strcpy(char *, char *);
 char *_strcat(char *, const char *);
 char *_strdup(const char *);
 int _putchar(char);
 unsigned int _strspn(char *s, char *accept);
 
-/* utils_funcs3.c */
+/* helpers_3.c */
 int _atoi(const char *str);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *dest, char *src, unsigned int n);
