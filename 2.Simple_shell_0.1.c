0a823d59eacec3181aecb65196c3043f19a6824f
NChaouch <nourchaouch44@gmail.com>
2024-04-23 21:37:00 +0200

    perror changed by /.hsh

--- 2.Simple_shell_0.1.c
+++ 2.Simple_shell_0.1.c
@@ -11,13 +11,54 @@
  * @buffer: Buffer to store the input command.
  * @read_result: Pointer to store the result of the read operation.
  */
-void read_command(char *buffer, ssize_t *read_result);
+void read_command(char *buffer, ssize_t *read_result)
+{
+	size_t length;
+
+	*read_result = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+
+	if (*read_result < 0)
+	{
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+
+	length = (size_t)(*read_result);
+
+	if (buffer[length - 1] == '\n')
+	{
+		buffer[length - 1] = '\0';
+	}
+}
 
 /**
  * execute_command - Execute a command.
  * @buffer: Buffer containing the command to execute.
  */
-void execute_command(char *buffer);
+void execute_command(char *buffer)
+{
+	pid_t pid = fork();
+
+	if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		char *args[2];
+
+		args[0] = buffer;
+		args[1] = NULL;
+		execve(args[0], args, __environ);
+		perror("./hsh");
+		_exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
 
 /**
  * main - Entry point of the simple shell program.
@@ -60,59 +101,12 @@ int main(void)
 	return (EXIT_SUCCESS);
 }
 
-/**
- * read_command - Read a command from standard input.
- * @buffer: Buffer to store the input command.
- * @read_result: Pointer to store the result of the read operation.
- */
-void read_command(char *buffer, ssize_t *read_result)
-{
-	size_t length;
 
-	*read_result = read(STDIN_FILENO, buffer, BUFFER_SIZE);
 
-	if (*read_result < 0)
-	{
-		perror("read");
-		exit(EXIT_FAILURE);
-	}
 
-	length = (size_t)(*read_result);
 
-	if (buffer[length - 1] == '\n')
-	{
-		buffer[length - 1] = '\0';
-	}
-}
 
-/**
- * execute_command - Execute a command.
- * @buffer: Buffer containing the command to execute.
- */
-void execute_command(char *buffer)
-{
-	pid_t pid = fork();
 
-	if (pid < 0)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		char *args[2];
-
-		args[0] = buffer;
-		args[1] = NULL;
-		execve(args[0], args, __environ);
-		perror(buffer);
-		_exit(EXIT_FAILURE);
-	}
-	else
-	{
-		wait(NULL);
-	}
-}
 
 
 
