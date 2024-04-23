eca6cdbf8d9165444c02173ad77369b703a28780
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-23 13:57:06 +0000

    xeme test

--- main.c
+++ main.c
@@ -1,62 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "main.h"
 
-#define MAX_COMMAND_LENGTH 100
+/**
+ * main - Init of prompt.
+ * @av: Stdin fd stocking args.
+ * @ac: Arguments number.
+ * Return: 0.
+*/
 
-void display_prompt(void)
+int main(int ac, char **argv)
 {
-	printf("MyShell$ ");
-	fflush(stdout);
-}
-
-int main()
-{
-	char command[MAX_COMMAND_LENGTH];
-	pid_t pid;
+	char *prompt = "Guantanamo$ ", *token;
+	char *buffer = NULL, *buffer_cpy = NULL;
+	size_t nbytes = 0;
+	ssize_t nchar_read;
+	const char *delim = " \n";
+	int n_token = 0, i = 0;
+	(void)ac;
 
 	while (1)
 	{
-		display_prompt();
 
-		if (fgets(command, sizeof(command), stdin) == NULL)
+		printf("%s", prompt);
+		nchar_read = getline(&buffer, &nbytes, stdin);
+
+		if (nchar_read == -1)
 		{
-			printf("\n");
-			break;
+			printf("Exit shell\n");
+			return (-1);
 		}
 
-		command[strcspn(command, "\n")] = '\0';
+		buffer_cpy = malloc(sizeof(char) * nchar_read);
 
-		pid = fork();
-
-		if (pid == -1)
+		if (buffer_cpy == NULL)
 		{
-			perror("fork");
-			exit(EXIT_FAILURE);
+			perror("Guantanamo: cell allocation error");
+			return (-1);
 		}
-		else if (pid == 0)
-		{
-			if (execlp(command, command, (char *)NULL) == -1)
-			{
-				fprintf(stderr, "Command not found: %s\n", command);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
+		
+		strcpy(buffer_cpy, buffer);
 
-			int status;
-
-			if (waitpid(pid, &status, 0) == -1)
-			{
-				perror("waitpid");
-				exit(EXIT_FAILURE);
-			}
+		token = strtok(buffer, delim);
+		 
+		while (token != NULL)
+		{
+			n_token++;
+			token = strtok(NULL, delim);
 		}
+
+		n_token++;
+		
+		argv = malloc(sizeof(char *) * n_token);
+		
+		token = strtok(buffer_cpy, delim);
+		
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
+
+		execmd(argv);
 	}
-
-	return EXIT_SUCCESS;
+	
+	free(buffer_cpy);
+	free(buffer);
+	
+	return (0);
 }
