8906d53106eab392b03cc4bbd8c954bbfd40171e
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-23 18:15:55 +0000

    again

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int ac, char **argv);
+
+#endif
