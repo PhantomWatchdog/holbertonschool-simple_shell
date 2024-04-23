4e50c1d85b288bcdf32303b472859bebe1dc33a5
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-23 15:25:53 +0000

    New test

--- main.c
+++ main.c
@@ -1,71 +1,33 @@
 #include "main.h"
 
 /**
- * main - Init of prompt.
- * @av: Stdin fd stocking args.
- * @ac: Arguments number.
+ * main - Prompt core.
  * Return: 0.
-*/
+ */
 
-int main(int ac, char **argv)
+int main(void)
 {
-	char *prompt = "Guantanamo$ ", *token;
-	char *buffer = NULL, *buffer_cpy = NULL;
-	size_t nbytes = 0;
-	ssize_t nchar_read;
-	const char *delim = " \n";
-	int n_token = 0, i = 0;
-	(void)ac;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	int read_bytes;
 
 	while (1)
 	{
+		prompt(); /* Affiche le prompt du shell*/
 
-		printf("%s", prompt);
-		nchar_read = getline(&buffer, &nbytes, stdin);
+		/* Lire la commande entrée par l'utilisateur*/
+		read_bytes = getline(&buffer, &bufsize, stdin);
 
-		if (nchar_read == -1)
+		if (read_bytes == -1)
 		{
-			printf("Exit shell\n");
-			return (-1);
+			break; /* Quitter si getline() échoue*/
 		}
 
-		buffer_cpy = malloc(sizeof(char) * nchar_read);
-
-		if (buffer_cpy == NULL)
-		{
-			perror("Guantanamo: cell allocation error");
-			return (-1);
-		}
-		
-		strcpy(buffer_cpy, buffer);
-
-		token = strtok(buffer, delim);
-		 
-		while (token != NULL)
-		{
-			n_token++;
-			token = strtok(NULL, delim);
-		}
-
-		n_token++;
-		
-		argv = malloc(sizeof(char *) * n_token);
-		
-		token = strtok(buffer_cpy, delim);
-		
-		for (i = 0; token != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(argv[i], token);
-			token = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-
-		execmd(argv);
+		execute_command(buffer); /* Exécuter la commande saisi*/
 	}
-	
-	free(buffer_cpy);
-	free(buffer);
-	
+
+	free(buffer); /* Libérer la mémoire allouée pour le buf*/
+
 	return (0);
 }
+
