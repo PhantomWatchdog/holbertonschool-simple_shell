31146c47dd336288859ff0b133ce8e5be91173d0
PhantomWatchdog <8706@holbertonstudents.com>
2024-04-24 15:59:40 +0000

    poiiiints

--- execute.c
+++ execute.c
@@ -29,14 +29,13 @@ int execute_cmd(char *usr_input)
 		}
 		args[count] = NULL;
 		
-		if (strcmp(usr_input, args[0]) == 0)
-		{
+		/*if (strcmp(usr_input, args[0]) == 0){*/
 			if (execve(args[0], args, NULL) == -1)
 			{
 				perror("./hsh");
 				exit(1);
 			}
-		}
+		/*}*/
 		else
 		{
 			exit(1);
